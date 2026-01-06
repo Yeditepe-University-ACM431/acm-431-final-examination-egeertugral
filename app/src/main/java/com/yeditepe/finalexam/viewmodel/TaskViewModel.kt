46114e3cde1526ea830aa85db2f28a66be0d9c32
egeertugral <ertugralege@gmail.com>
2026-01-06 15:10:43 +0300

    Initialize mutable task list in TaskViewModel
    Added a mutable state list of Task objects with initial tasks.

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -4,10 +4,24 @@ import androidx.lifecycle.ViewModel
 
 class TaskViewModel : ViewModel() {
 
+    private val _tasks = mutableStateListOf(
+        Task(1, "Submit Final Project", false),
+        Task(2, "Study for Engin Kandıran's Hard Exam", true),
+        Task(3, "Present Final Project to our kind Teacher", true)
+    )
+    val tasks: List<Task> = _tasks
+
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
-
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val index = _tasks.indexOfFirst { it.id == taskId }
+        if (index != -1) {
+            val task = _tasks[index]
+            _tasks[index] = task.copy(isCompleted = !task.isCompleted)
+        }
     }
+
 }
+}
+
